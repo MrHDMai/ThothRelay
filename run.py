2d310b31ae366ff30e3a30bfb62da070080165fc
MrHDMai <m.haodmai@gmail.com>
2025-08-21 22:22:45 -0700

    Redis Configuration

--- run.py
+++ run.py
@@ -1,12 +1,15 @@
-# app.py
 from flask import Flask
 import redis
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    r = redis.Redis(host='localhost', port=6379)
-    print(r.ping())
+    try:
+        r = redis.Redis(host='localhost', port=6379)
+        return f"Redis says: {r.ping()}"
+    except redis.ConnectionError:
+        return "Cannot connect to Redis. Is it running?"
 
 if __name__ == '__main__':
     app.run(debug=True)
